8426e54d5bb602ac3bd04ea1d3da4ada5d4663aa
Shodlik Shomuratov <shodlikcoder@gmail.com>
2023-10-18 16:39:57 +0500

    Call stack added.

--- Call Stack/index.js
+++ Call Stack/index.js
@@ -16,3 +16,16 @@ greeting();
 // 5. Call stack executes sayHi() function and takes it off from the stack
 // 6. Then call stack gets back to the greeting() function and executes the rest of it.
 // 7. After executing, call stack also takes off the greeting function from the stack
+
+function foo () {
+    foo();
+}
+
+foo();
+
+// This statement create an stack overflow error like this because of the range of the call stack:
+// 
+// /home/shodlik/Documents/CODING/33-js-concepts/Call Stack/index.js:21
+//     foo()
+//     ^
+// RangeError: Maximum call stack size exceeded
